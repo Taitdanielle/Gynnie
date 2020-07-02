8c7e9bdc284ab4f649e3a9b62f19c1f11046103e
Danielle Tait <Danielletait@hotmail.co.uk>
2020-07-02 22:05:53 +0000

    .

--- app.py
+++ app.py
@@ -8,6 +8,11 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DB_NAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")    
 
+@app.route('/')
+def index():
+    return render_template("base.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=os.environ.get("PORT"),
