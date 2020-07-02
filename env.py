0569817cbd0faa2f1151a1688398384ce3da0193
Danielle Tait <Danielletait@hotmail.co.uk>
2020-07-02 15:21:57 +0000

    added env mogo uri, secret key

---
+++ env.py
@@ -0,0 +1,5 @@
+import os   
+
+os.environ["MONGO_DBNAME"] = "GynnieDB"
+os.environ["SECRET_KEY"] = "Tait.danielle.123"
+os.environ["MONGO_URI"] = "mongodb+srv://root:r00tUser@myfirstcluster-zzb40.mongodb.net/GynnieDB?retryWrites=true&w=majority"
