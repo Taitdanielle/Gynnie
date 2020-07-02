0edbaa2240f2879a9963e3c63fe3ec4af6f57eea
Danielle Tait <Danielletait@hotmail.co.uk>
2020-07-02 22:22:08 +0000

    corrected spelling of gitignore file

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-import os 
+import os
+from flask import Flask, render_template, request, flash, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
