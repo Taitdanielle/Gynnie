51664e16e65851a256d776e8397e4eb1c6aba546
Danielle Tait <Danielletait@hotmail.co.uk>
2020-07-05 19:53:54 +0000

    added register form

--- form.py
+++ form.py
@@ -1,7 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired, Length
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username'), validators=[DaraRequired()])
+    username = Stringfield('Username', validators=[DataRequired(), Length(min=2, max=20)])
+
+    email = StringField('Email', validators=[DataRequired(), Email()])
+
+    password = PasswordField('Password', validators[])
+    confirm_password = PasswordField('Confirm Password', validators[DataRequired(), EqualTo('password')])
+
+    submit = Submit
