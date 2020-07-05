8b5a359e607fa441fc6d3a96886af6cd0764e656
Danielle Tait <Danielletait@hotmail.co.uk>
2020-07-05 22:56:29 +0000

    re added eny.py

--- app.py
+++ app.py
@@ -1,41 +1,52 @@
 import os
-from flask import Flask, render_template, request, flash, url_for
-from form import RegistrationForm, LoginForm
+from flask import Flask, render_template, request, flash, session, redirect, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 if os.path.exists("env.py"):
-import env
+    import env
 
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DB_NAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
+
 mongo = PyMongo(app)
 
+@app.route('/')
+def index():
+    if 'username' in session:
+        return 'You are logged in as ' + session['username']
 
-@app.route('/landing.html')
-def landing():
-    return render_template('landing.html')
+    return render_template('base.html')
 
-@app.route('/home')
-def home():
-    return render_template('home.html')
-
-@app.route('/contact.html')
-def contact():
-    return render_template('contact-form.html')
-
-@app.route('/register')
-def register():
-    form = RegistrationForm()
-    return render_template('register-form.html', title='Register', form=form)
-
-@app.route('/login')
+@app.route('/login', methods=['POST'])
 def login():
-    form = LoginForm()
-    return render_template('login-form.html', title='Login', form=form)
+    users = mongo.db.users
+    login_user = users.find_one({'name' : request.form['username']})
 
+    if login_user:
+        if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+
+    return 'Invalid username/password combination'
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        users = mongo.db.users
+        existing_user = users.find_one({'name' : request.form['username']})
+
+        if existing_user is None:
+            hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
+            users.insert({'name' : request.form['username'], 'password' : hashpass})
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+        
+        return 'That username already exists!'
+
+    return render_template('register-form.html')
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
