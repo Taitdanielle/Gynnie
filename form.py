1fb43d88cebf5104754fb63053463bcbb2f96815
Danielle Tait <Danielletait@hotmail.co.uk>
2020-07-05 19:54:02 +0000

    added log in form

--- form.py
+++ form.py
@@ -1,14 +1,22 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
 class RegistrationForm(FlaskForm):
-    username = Stringfield('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
 
-    email = StringField('Email', validators=[DataRequired(), Email()])
 
-    password = PasswordField('Password', validators[])
-    confirm_password = PasswordField('Confirm Password', validators[DataRequired(), EqualTo('password')])
-
-    submit = Submit
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
