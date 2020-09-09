6d7ff10ac830845f767f8c3a514e25aebb96cdc3
Danielle Tait <Danielletait@hotmail.co.uk>
2020-09-09 00:22:04 +0000

    corrected link up error with contact route

--- app.py
+++ app.py
@@ -77,18 +77,29 @@ def mycocktails():
 
 # Contact route
 
-@app.route('/contactus', methods=["GET", "POST"])
+
+@app.route('/contact', methods=["GET", "POST"])
 def contact():
+    """
+    Renders the contact page and flashes responses based on inputs.
+    """
     try:
         users = mongo.db.users
         if request.method == "POST":
             flash("Thanks {} we have received your message! A member of our team will be in touch shortly".format(
                 request.form["name"]))
-    return render_template("components/forms/contact-form.html", page_title="Contact Us")
-
+        return render_template("components/forms/contact-form.html",
+                               body_id="contact-page", page_title="Contact Us",
+                               current_user=users.find_one({'name': session['username']}))
+    except:
+        if request.method == "POST":
+            flash("Thanks {} we have received your message! A member of our team will be in touch shortly".format(
+                request.form["name"]))
+        return render_template("components/forms/contact-form.html",
+                               body_id="contact-page", page_title="Contact Us")
 # Add Review route
 
-@app.route('/review/add/<drink_id>', methods=["GET", "POST"])
+@ app.route('/review/add/<drink_id>', methods = ["GET", "POST"])
 def add_review(drink_id):
     """
     Adds user review into the database.
@@ -98,44 +109,44 @@ def add_review(drink_id):
         'review': request.form.get('review'),
         'beer_id': ObjectId(drink_id),
     })
-    return redirect(url_for('cocktail_page', drink_id=drink_id))    
+    return redirect(url_for('cocktail_page', drink_id=drink_id))
 
 # Edit Review page
 
-@app.route('/review/edit/<review_id>', methods=["GET", "POST"])
+@ app.route('/review/edit/<review_id>', methods = ["GET", "POST"])
 def edit_review(review_id):
 
     if request.method == 'POST':
         mongo.db.reviews.update({'_id': ObjectId(review_id)}, {
             '$set': {'review': request.form.get('review')}})
         return redirect(url_for('edit_review', review_id=review_id))
-    users = mongo.db.users
-    review = mongo.db.reviews.find({"_id": ObjectId(review_id)})
+    users=mongo.db.users
+    review=mongo.db.reviews.find({"_id": ObjectId(review_id)})
     return render_template("pages/edit-review-form.html",
-                           body_id="edit-review-page", review=review, review_id=review_id,
-                           current_user=users.find_one({'name': session['username']}))
+                           body_id = "edit-review-page", review = review, review_id = review_id,
+                           current_user = users.find_one({'name': session['username']}))
 
 
-@app.route('/cocktail/<drink_id>')
+@ app.route('/cocktail/<drink_id>')
 def cocktail_page(drink_id):
     """
     Constructs page for single cocktail and
     makes reviews for front end.
     """
-    users = mongo.db.users
-    the_beer = mongo.db.cocktail.find_one({"_id": ObjectId(drink_id)})
-    you_might_like = mongo.db.cocktail.find().limit(3)
-    test = mongo.db.reviews.find({'drink_id': ObjectId(drink_id)})
-    reviews = []
-    current_user_obj = users.find_one({'name': session['username'].lower()})
+    users=mongo.db.users
+    the_beer=mongo.db.cocktail.find_one({"_id": ObjectId(drink_id)})
+    you_might_like=mongo.db.cocktail.find().limit(3)
+    test=mongo.db.reviews.find({'drink_id': ObjectId(drink_id)})
+    reviews=[]
+    current_user_obj=users.find_one({'name': session['username'].lower()})
     reviews.append(i)
     return render_template('pages/cocktails/cocktail-page.html',
-                           cocktail_reviews=reviews,
-                           body_id="drink-product", current_user=users.find_one(
+                           cocktail_reviews = reviews,
+                           body_id = "drink-product", current_user = users.find_one(
                                {'name': session['username']}))
 
 
 if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"),
+    app.run(host = os.environ.get("IP"),
             port=os.environ.get("PORT"),
             debug=os.environ.get("DEBUG"))
